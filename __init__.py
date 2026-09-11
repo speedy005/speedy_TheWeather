442404b32719927c2dd152b6bf65887cdc92ab3d
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-11 18:34:37 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ import os
 # VERSION / AUTHOR
 # ============================================================================
 
-__version__ = "1.2.8"
+__version__ = "1.2.9"
 VERSION = __version__
 
 _AUTHOR_ = "by speedy005 - 2026"
