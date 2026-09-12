e745c1b6c4056f892be858669208e9932fea3433
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-12 21:03:30 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# v.1.3.3
+# v.1.3.4
 # Original work by Caught
 # https://www.linuxsat-support.com/cms/user/40812-caught/
 # Modified by speedy005
@@ -159,7 +159,7 @@ def getCoordsFromEntry(value):
             return None, None
     return None, None
 
-version = '1.3.3'
+version = '1.3.4'
 
 UPDATE_RAW_BASE = "https://raw.githubusercontent.com/speedy005/speedy_TheWeather/master"
 UPDATE_PLUGIN_URL = UPDATE_RAW_BASE + "/plugin.py"
@@ -1179,7 +1179,26 @@ class sevendays(Screen):
 
         global weatherData
         dataDagen = weatherData["days"]
+        # ------------------------------------------------------------
+        # Windrichtung für die obere Anzeige
+        # ------------------------------------------------------------
+        winddir_top = "na"
 
+        try:
+            if dataDagen:
+                winddir_top = str(
+                    dataDagen[0].get("winddirection") or "na"
+                )
+
+                if winddir_top == "na":
+                    hours = dataDagen[0].get("hours", [])
+                    if hours:
+                        winddir_top = str(
+                            hours[0].get("winddirection") or "na"
+                        )
+
+        except Exception:
+            winddir_top = "na"
         self.selected = 0
         self.hourStep = 1
 
@@ -1429,7 +1448,8 @@ class sevendays(Screen):
                 <widget render="Label" source="bigtemp1" position="870,122" size="353,118" zPosition="3" valign="center" halign="left" font="Regular;108" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
                 <widget render="Label" source="bigweathertype1" position="870,298" size="480,40" zPosition="3" valign="center" halign="left" font="Regular;28" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
                 <widget render="Label" source="GevoelsTemp1" position="870,250" size="354,40" zPosition="3" valign="center" halign="left" font="Regular;28" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
-                <widget render="Label" source="winddir1" position="870,346" size="345,40" zPosition="3" valign="center" halign="left" font="Regular;28" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
+                <widget render="Label" source="winddir1" position="870,346" size="330,45" zPosition="3" valign="center" halign="left" font="Regular;28" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
+                <widget name="winddiricon1" position="1235,326" size="56,56" zPosition="4" alphatest="blend" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/speedy_TheWeather/""" + icoonpath + """/windhd/""" + winddir_top + """.png"/>
                 <widget name="weatheralertbg1" position="1322,240" size="588,72" zPosition="2" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/speedy_TheWeather/""" + SHARED_PACK + """/alert/vlak_alert.png" alphatest="on"/>
                 <widget name="weatheralerticon1" position="1332,244" size="64,64" zPosition="4" alphatest="blend" transparent="1"/>
                 <widget name="weatheralert1" position="1440,244" size="576,64" zPosition="3" valign="center" halign="left" font="Regular;48" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
@@ -1656,6 +1676,7 @@ class sevendays(Screen):
                 <widget render="Label" source="bigweathertype1" position="565,208" size="320,30" zPosition="3" valign="center" halign="left" font="Regular;18" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
                 <widget render="Label" source="GevoelsTemp1" position="565,176" size="236,30" zPosition="3" valign="center" halign="left" font="Regular;18" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
                 <widget render="Label" source="winddir1" position="565,240" size="230,30" zPosition="3" valign="center" halign="left" font="Regular;18" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
+                <widget name="winddiricon1" position="795,240" size="28,28" zPosition="4" alphatest="blend" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/speedy_TheWeather/""" + icoonpath + """/windhd/""" + winddir_top + """.png"/>
                 <widget name="weatheralertbg1" position="877,162" size="398,48" zPosition="2" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/speedy_TheWeather/""" + SHARED_PACK + """/alert/vlak_alertsd.png" alphatest="on"/>
                 <widget name="weatheralerticon1" position="883,165" size="42,42" zPosition="4" alphatest="blend" transparent="1"/>
                 <widget name="weatheralert1" position="955,165" size="340,42" zPosition="3" valign="center" halign="left" font="Regular;32" foregroundColor="#00ffffff" backgroundColor="#00202020" transparent="1" shadowColor="black" shadowOffset="-2,-2"/>
@@ -1707,7 +1728,7 @@ class sevendays(Screen):
         self["bigweathertype1"] = StaticText()
         self["GevoelsTemp1"] = StaticText()
         self["winddir1"] = StaticText()
-
+        self["winddiricon1"] = Pixmap()
         self["weatheralertbg1"] = Pixmap()
         self["weatheralerticon1"] = Pixmap()
         self["weatheralert1"] = Label("")
