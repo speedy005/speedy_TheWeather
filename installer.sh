095c37328368cb31ea2c4ed69608b34ebd4bb0e0
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-12 18:32:43 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 # speedy_TheWeather Installer
 # =========================================================
 
-version='1.3.2'
+version='1.3.3'
 
 changelog='Fix malformed locale language file. Added an update function. Fixed weather icons .Fixed detached GUI restart. Buy me a coffee if you like this plugin.'
 
