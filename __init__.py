47e771b2a3c4d42e624f90a67d5aa005a6815061
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-20 15:05:01 +0200

    Update version from 1.5.1 to 1.5.4

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ import os
 # VERSION / AUTHOR
 # ============================================================================
 
-__version__ = "1.5.1"
+__version__ = "1.5.4"
 VERSION = __version__
 
 _AUTHOR_ = "by speedy005 - 2026"
