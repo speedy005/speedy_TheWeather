fa194a3c4077113e96a10ab9589084cc513cd46f
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-09-24 17:02:14 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 # speedy_TheWeather Installer
 # =========================================================
 
-version='1.5.7'
+version='1.5.8'
 
 changelog='Fixed malformed locale language file. Fix language po and mo name.Added an update function. Fixed Rain Radar Screen. Fixed the date display in the Seven Day Weather Screen. Fixed weather icons. Fixed detached GUI restart. Added customizable color settings. Buy me a coffee if you like this plugin.'
 
